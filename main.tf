b43ba48a41561bc284de08c3eb185ebb54b6f1df
JeonghwanSa <jeonghwansa@jobis.co>
2023-11-20 18:56:19 +0900

    fix typo

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_ownership_controls" "bucketownership" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "publcaccess_block" {
+resource "aws_s3_bucket_public_access_block" "publicaccess_block" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
 
   block_public_acls       = true
